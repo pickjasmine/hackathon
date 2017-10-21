06fd0bea091be958967170a7e834035394f3a911
CardboardHeart <qmsalas@iastate.edu>
2017-10-21 01:47:13 -0500

    in progress: chaining get/post requests

--- index.js
+++ index.js
@@ -7,31 +7,41 @@ app.get('/', function (req, res) {
   res.send('Hello World!')
 });
 
-app.listen(8080, function () {
+app.listen(8080, function() {
   console.log('Example app listening on port 8080!')
 });
 
-function test() {
-  return got.post('https://api.mixmax.com/v1/send',{
-    body: JSON.stringify({
+async function getJSON() {
+  const result = await got("https://jsonplaceholder.typicode.com/posts/1")
+  .then(response => {
+    let data = JSON.parse(response.body);
+    result = data.title;
+    console.log(result);
+    return result
+  });
+}
 
+async function test() {
+  const title = await getJSON();
+  console.log(title);
+  return got.post('https://api.mixmax.com/v1/send', {
+    headers: {
+      'content-type': "application/json",
+      'X-API-TOKEN': token
+    },
+    body: JSON.stringify({
       "message": {
         "to": "musikmann7448@gmail.com",
-        "subject": "This is my subject",
-        "html" : "test"
+        "subject": "This is a new test subject",
+        "html": title
       }
     }),
-      headers: {
-        'content-type': "application/json",
-        'X-API-TOKEN': token
-      }
-    }
-  )
+  }
+)
+.then(response => {
+  console.log(response.statusCode);
+  })
 }
-/*.then(response => {
-let result = JSON.parse(response.body)
-console.log(result.body);
-});
-}
-*/
-test().catch(console.log);
+
+test()
+.catch(console.log);
