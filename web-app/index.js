21d9bf8ff954fd289cf7828ec8e262b31693af4a
pickjasmine <francois.jasminem@gmail.com>
2017-10-21 08:31:34 -0500

    changes to css

--- web-app/index.js
+++ web-app/index.js
@@ -11,25 +11,39 @@ function toggleSlack() {
 }
 
 
-$(document).ready(function() {
+$(document).ready(function () {
+    let keywordCount = 0;
 
-   $('#addKeyword').click(function () {
-       let keyword = $('#keywordInput').val();
-       let keywordId = keyword.toLowerCase();
-       $('#keywordList').append('<li id="' + keywordId + '" class="draggable droppable ui-widget-content">' + keyword + '</li>');
-       $('#keywordInput').val("");
-       $('.draggable').draggable({
-           opacity: 0.7,
-           containment: '.leftSide',
-           cursor: 'crosshair',
-           helper: 'clone',
-           grid: [ 20, 20 ]
-       });
-        $('.droppable').droppable({
-            drop: function(event, ui) {
-                ui.draggable.remove();
-            }
-        });
-   });
+    $('#addKeyword').click(function () {
+        if (keywordCount < 3) {
+            let keyword = $('#keywordInput').val();
+            let keywordId = keyword.toLowerCase();
+            $('#keywordList').append('<li id="' + keywordId + '" class="draggable droppable ui-widget-content">' + keyword + '</li>');
+            keywordCount++;
+            $('#keywordInput').val("");
+            $('.draggable').draggable({
+                opacity: 0.7,
+                containment: '.leftSide',
+                cursor: 'move',
+                helper: 'clone',
+                grid: [30, 30],
+                cursorAt: {bottom: 0, right: 0}
+            });
+        } else {
+            $('#keywordInput').val('Sorry, 3 keywords max');
+        }
+
+    });
+
+    $('.droppable').droppable({
+        drop: function (event, ui) {
+            ui.draggable.remove();
+            keywordCount--;
+        }
+    });
+
+    $('#submit').click(function() {
+
+    })
 
 });
