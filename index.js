ebaf23f10524bb99554b3003d8bf020a2a5e3174
pickjasmine <francois.jasminem@gmail.com>
2017-10-21 02:11:48 -0500

    revert because i'm still learning Revert "in progress: chaining get/post requests"
    This reverts commit 06fd0bea091be958967170a7e834035394f3a911.

--- index.js
+++ index.js
@@ -7,41 +7,31 @@ app.get('/', function (req, res) {
   res.send('Hello World!')
 });
 
-app.listen(8080, function() {
+app.listen(8080, function () {
   console.log('Example app listening on port 8080!')
 });
 
-async function getJSON() {
-  const result = await got("https://jsonplaceholder.typicode.com/posts/1")
-  .then(response => {
-    let data = JSON.parse(response.body);
-    result = data.title;
-    console.log(result);
-    return result
-  });
-}
-
-async function test() {
-  const title = await getJSON();
-  console.log(title);
-  return got.post('https://api.mixmax.com/v1/send', {
-    headers: {
-      'content-type': "application/json",
-      'X-API-TOKEN': token
-    },
+function test() {
+  return got.post('https://api.mixmax.com/v1/send',{
     body: JSON.stringify({
+
       "message": {
         "to": "musikmann7448@gmail.com",
-        "subject": "This is a new test subject",
-        "html": title
+        "subject": "This is my subject",
+        "html" : "test"
       }
     }),
-  }
-)
-.then(response => {
-  console.log(response.statusCode);
-  })
+      headers: {
+        'content-type': "application/json",
+        'X-API-TOKEN': token
+      }
+    }
+  )
 }
-
-test()
-.catch(console.log);
+/*.then(response => {
+let result = JSON.parse(response.body)
+console.log(result.body);
+});
+}
+*/
+test().catch(console.log);
