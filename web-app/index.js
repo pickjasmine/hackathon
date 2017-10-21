8af25faa0af81f99d1a393563188976b17146ae9
pickjasmine <francois.jasminem@gmail.com>
2017-10-21 18:30:36 -0500

    make javascript file of createJSON function and supporting functions

--- web-app/index.js
+++ web-app/index.js
@@ -10,67 +10,6 @@ function toggleSlack() {
     $('.showSlack').slideToggle('fast');
 }
 
-function getJobLevel() {
-    let jobLevel = [];
-    if ($('#entryLevel').is(':checked')) {
-        jobLevel.push('entryLevel');
-    }
-    if ($('#midLevel').is(':checked')) {
-        jobLevel.push('midLevel');
-    }
-    if ($('#seniorLevel').is(':checked')) {
-        jobLevel.push('seniorLevel');
-    }
-    return jobLevel;
-}
-
-function getJobType() {
-    let jobType = [];
-    if ($('#fullTime').is(':checked')) {
-        jobType.push('fullTime');
-    }
-    if ($('#partTime').is(':checked')) {
-        jobType.push('partTime');
-    }
-    return jobType;
-
-}
-
-function getContactMethod() {
-    let contactMethod = [];
-    if ($('#email').is(':checked')) {
-        contactMethod.push('email');
-    }
-    if ($('#text').is(':checked')) {
-        contactMethod.push('text');
-    }
-    if ($('#slack').is(':checked')) {
-        contactMethod.push('slack');
-    }
-    return contactMethod;
-}
-
-function getKeywords() {
-    let keywords = $('#keywords').val().split(",");
-    return keywords;
-}
-
-function createJSON() {
-    let formElements = {
-        location: $('#locationInput').val(),
-        searchRadius: $('#searchRadius').val(),
-        jobLevel: getJobLevel(),
-        jobType: getJobType(),
-        keywords: getKeywords(),
-        contactMethod: getContactMethod(),
-        email: $('#emailInput').val(),
-        phoneNumber: $('#phoneInput').val(),
-        slack: $('#slackInput').val()
-    };
-    return JSON.stringify(formElements);
-}
-
-
 $(document).ready(function () {
     let keywordCount = 0;
     let keywords = [];
@@ -79,7 +18,7 @@ $(document).ready(function () {
         if (keywordCount < 3) {
             let keyword = $('#keywordInput').val();
             let keywordId = keyword.toLowerCase();
-            $('#keywordList').append('<li id="' + keywordId + '" class="draggable droppable ui-widget-content">' + keyword + '</li>');
+            $('#keywordList').append('<li id="' + keywordId + '" class="key draggable droppable ui-widget-content">' + keyword + '</li>');
             keywords[keywordCount] = keywordId;
             keywordCount++;
             $('#keywordInput').val("");
@@ -94,7 +33,6 @@ $(document).ready(function () {
         } else {
             $('#keywordInput').val('Sorry, 3 keywords max');
         }
-
     });
 
     $('.droppable').droppable({
@@ -103,9 +41,4 @@ $(document).ready(function () {
             keywordCount--;
         }
     });
-
-    $('#submit').click(function () {
-        $('#keywords').val(keywords);
-        createJSON();
-    })
 });
