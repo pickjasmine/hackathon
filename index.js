a2c4c4f19443f1cf4dc6cc1d47379fb9e92911a0
Shivkarthi Sundar <ssundar@iastate.edu>
2017-10-21 05:45:18 -0500

    Reduced to one function, found that delay is much easier to implement than asynchronous await functions. code possibly not reliable in low internet speed areas

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const got  = require('got');
 const app = express();
-const token = "bcbf0854-97f5-4135-9224-d9a13a5b2dcf";
+const token = "964c73e3-d289-4cb7-a895-e9ebd1305b1d";
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
@@ -11,37 +11,30 @@ app.listen(8080, function() {
   console.log('Example app listening on port 8080!')
 });
 
-async function getJSON() {
-  const result = await got("https://jsonplaceholder.typicode.com/posts/1")
+function getBody() {
+  let newVar = null;
+  got("jsonplaceholder.typicode.com/posts/1")
   .then(response => {
     let data = JSON.parse(response.body);
-    result = data.title;
-    console.log(result);
-    return result
-  });
-}
-
-async function test() {
-  const title = await getJSON();
-  console.log(title);
-  return got.post('https://api.mixmax.com/v1/send', {
-    headers: {
-      'content-type': "application/json",
-      'X-API-TOKEN': token
-    },
-    body: JSON.stringify({
-      "message": {
-        "to": "musikmann7448@gmail.com",
-        "subject": "This is a new test subject",
-        "html": title
-      }
-    }),
-  }
-)
-.then(response => {
-  console.log(response.statusCode);
+    test = data.title;
+    newVar = test;
+    console.log(newVar)
   })
+  setTimeout(function() {
+    got.post("https://api.mixmax.com/v1/send", {
+      body: JSON.stringify({
+        "message": {
+          "to": "musikmann7448@gmail.com",
+          "subject": "Job listing",
+          "html": newVar + "test body"
+        }
+      }),
+      headers: {
+        "content-type": "application/json",
+        "X-API-TOKEN": token
+      }
+    })
+  }, 3000);
 }
 
-test()
-.catch(console.log);
+getBody();
