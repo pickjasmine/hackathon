49d0f83b204d8c9907d7c789d32e54f17f4ec6b9
CardboardHeart <qmsalas@iastate.edu>
2017-10-21 19:09:15 -0500

    setup express endpoint for posts

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ const express = require('express');
 const app = express();
 const got  = require('got');
 const fs = require('fs');
-const cheerio = require('cheerio');
-const $ = cheerio.load(fs.readFileSync("./web-app/index.html"));
+const parser = require('body-parser');
 const token = "964c73e3-d289-4cb7-a895-e9ebd1305b1d";
 
 app.set('port', 8080);
 app.use(express.static('web-app'));
+app.use(parser.json());
+app.post('/submit', function(req, res) {
+  res.send('success!')
+  console.log(req.body);
+})
 app.listen(8080, function() {
   console.log('Server started on port 8080!')
 });
@@ -17,34 +21,32 @@ app.listen(8080, function() {
 
 let github_url="https://jobs.github.com/positions.json?description=python&location=sf&full_time=true";
 
-getJobs(github_url)
-.then(sendEmail)
-.catch((err) => {
-  console.log("Oh no! Something went wrong!");
-})
-
-// TODO: Use Cheerio to Grab From Data
-// keywords = description [in url]
-// location = location [in url]
-// job type = if full time, full_time=true [in url]
+// getJobs(github_url)
+// .then(sendEmail)
+// .catch((err) => {
+//   console.log("Oh no! Something went wrong!");
+//   console.log(err);
+// })
 
 async function getJobs(url) {
   let json;
   let jobs = [];
+  let job;
 
-  await got(github_url)
-  .then(response => {
-    let data = JSON.parse(response.body);
-    for (let i = 0; i < data.length; i++) {
-      jobs.push(data[i].title, data[i].company, data[i].location, data[i].url);
-    }
-  })
+  let response = await got(github_url)
+
+  let data = JSON.parse(response.body);
+  for (let i = 0; i < data.length; i++) {
+    let { title, location, company, url } = data[i];
+    jobs.push({
+      title, location, company, url
+    });
+  }
   return jobs;
 }
 
 async function sendEmail(json) {
-  const result = await getJobs();
-
+  const jobs = await getJobs();
   await got.post("https://api.mixmax.com/v1/send", {
     headers: {
       "content-type": "application/json",
@@ -54,7 +56,9 @@ async function sendEmail(json) {
       "message": {
         "to": "musikmann7448@gmail.com",
         "subject": "Job Listing",
-        "html": "hey this is just a quick test"
+        "html": jobs.map(function(job) {
+          return `${job.title} ${job.location} ${job.company} ${job.url}`
+        }).join("<br/>")
       }
     })
   })
